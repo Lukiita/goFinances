3e16ff416105411b0df4845d22393fbf3d73e47f
Lukita <lucas.liima030@gmail.com>
2022-08-18 13:03:32 -0300

    feat: adiciona sessão de usuário do header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Container, Header } from './styles';
+
+import {
+  Container,
+  Header, Icon, Photo,
+  User,
+  UserGreeting, UserInfo, UserName, UserWrapper
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Header/>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://avatars.githubusercontent.com/u/11157950?v=4' }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Lucas</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name='power' />
+        </UserWrapper>
+
+      </Header>
     </Container>
   )
 }
