c83bb395e12e296910745a3fcb1c6da5462828f2
Lukita <lucas.liima030@gmail.com>
2022-08-20 16:22:36 -0300

    feat: adiciona botão de transação

---
+++ src/components/Forms/TransactionTypeButton/styles.ts
@@ -0,0 +1,45 @@
+import { Feather } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled, { css } from 'styled-components/native';
+
+interface TransactionProps {
+  type: 'up' | 'down';
+  isActive: boolean;
+}
+
+export const Container = styled(TouchableOpacity) <TransactionProps>`
+  width: 48%;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  border: 1.5px solid ${({ theme }) => theme.colors.text_light};
+  border-radius: 5px;
+  padding: 16px;
+
+  ${({ isActive, type }) => isActive && type === 'up' && css`
+    border: none;
+  
+    background-color: ${({ theme }) => theme.colors.success_light};
+  `}
+
+  ${({ isActive, type }) => isActive && type === 'down' && css`
+    border: none;
+
+    background-color: ${({ theme }) => theme.colors.attention_light};
+  `}
+`;
+
+export const Icon = styled(Feather) <TransactionProps>`
+    font-size: ${RFValue(24)}px; 
+    margin-right: 12px;
+
+    color: ${({ theme, type }) => type === 'up' ? theme.colors.success : theme.colors.attention};
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px; 
+`;
