37e75dcadae7962198aa4388cf7590c05e141646
Lukita <lucas.liima030@gmail.com>
2022-08-22 10:06:31 -0300

    feat(components): adiciona ControlledInput

---
+++ src/components/Forms/ControlledInput/index.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Control, Controller } from 'react-hook-form';
+import { TextInputProps } from 'react-native';
+import { Input } from '../Input';
+import { Container } from './styles';
+
+interface Props extends TextInputProps {
+  control: Control;
+  name: string;
+}
+
+export function ControlledInput(
+  {
+    control,
+    name,
+    ...rest
+  }: Props) {
+  return (
+    <Container>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            onChangeText={onChange}
+            value={value}
+            {...rest} />
+        )}
+      />
+
+    </Container>
+
+  );
+}
